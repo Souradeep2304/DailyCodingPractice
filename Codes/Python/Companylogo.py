998d507f7da5a7e2c0fac3eacd2783d994264d0b
souradeep2304 <souradeepbanerjee2304@gmail.com>
2020-07-04 20:36:50 +0530

    code updated

--- Codes/Python/Companylogo.py
+++ Codes/Python/Companylogo.py
@@ -7,7 +7,9 @@ import re
 import sys
 import operator
 
+"""
 
+# implementation works for small inputs
 if __name__ == '__main__':
     s = input()
     l=len(set(s)) #no of unique characters
@@ -36,14 +38,41 @@ if __name__ == '__main__':
         items[s[i]]=c+1
         i=i+1
         c=0
-    final=dict(sorted(items.items(), key=operator.itemgetter(1),reverse=True))
-    p=0 # to print first 3 most common characters
-    #for k in final:
-     #   print(k+" "+str(final[k]))
-      #  p=p+1
-       # if(p>=3):
-        #    break
+    
+    #res = all(list(items.values())[0] == x for x in items.values())
+    res = False
+    if(res==True):
+        p=0 # to print first 3 most common characters
+        for k in sorted(items):
+            print(k+" "+str(items[k]))
+            p=p+1
+            if(p>=3):
+                break
 
-    print(items)
+
+    if(res == False):
+        final=dict(sorted(items.items(), key=operator.itemgetter(1),reverse=True))
+        p=0 # to print first 3 most common characters
+        for k in final:
+            print(k+" "+str(final[k]))
+            p=p+1
+            if(p>=3):
+                break
+"""
+
+
+from collections import Counter, OrderedDict
+
+
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+
+if __name__ == '__main__':
+    s = input()
+    [print(*c) for c in OrderedCounter(sorted(s)).most_common(3)]
+
+
+        
     
 

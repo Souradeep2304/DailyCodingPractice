349b17359b00ca7aee86cd2c65420d4324ea54a8
souradeep2304 <souradeepbanerjee2304@gmail.com>
2020-07-04 20:47:29 +0530

    Daily code added

---
+++ Codes/Python/swapcase.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    s1=""
+    for i in s:
+        if i.islower() == True:
+            s1=s1+i.upper()
+        else:
+            s1=s1+i.lower()
+    return s1
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)

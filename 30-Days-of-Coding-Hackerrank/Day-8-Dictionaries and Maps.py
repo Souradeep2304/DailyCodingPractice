c367069adadfbbc51083a59f4822b9662eedc400
Souradeep2304 <souradeepbanerjee2304@gmail.com>
2020-08-26 19:00:16 +0530

    adding the daily code

--- 30-Days-of-Coding-Hackerrank/Day-8-Dictionaries and Maps.py
+++ 30-Days-of-Coding-Hackerrank/Day-8-Dictionaries and Maps.py
@@ -3,10 +3,12 @@ d=[input().split() for _ in range(n)]
 
 phonebook = {k: v for k,v in d}
 
-
-for i in range(0,n):
-    name=input()
-    if name in phonebook:
-        print('%s=%s' % (name, phonebook[name]))
-    else:
-        print('Not found')
+while True:
+    try:
+        name = input()
+        if name in phonebook:
+            print('%s=%s' % (name, phonebook[name]))
+        else:
+            print('Not found')
+    except:
+        break

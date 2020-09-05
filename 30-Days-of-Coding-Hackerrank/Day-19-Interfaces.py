dda5631ae52baa088095cf0be96ed817915babfe
souradeep2304 <souradeepbanerjee2304@gmail.com>
2020-09-05 11:03:08 +0530

    adding daily code

---
+++ 30-Days-of-Coding-Hackerrank/Day-19-Interfaces.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        i=1
+        s=0
+        while(i<=n):
+            if(n%i==0):
+                s=s+i
+            i=i+1
+        return s
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
